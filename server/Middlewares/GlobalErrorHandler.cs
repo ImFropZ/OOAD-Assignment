80bf66ca6d2e2d429973a1ce1c0011ac5955f4e9
FropZ <limtangmeng100@gmail.com>
2023-10-25 08:32:45 +0700

    Added: middlewares

---
+++ server/Middlewares/GlobalErrorHandler.cs
@@ -0,0 +1,25 @@
+namespace server.Middlewares
+{
+    public class GlobalErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync(e.Message);
+            }
+        }
+    }
+}
