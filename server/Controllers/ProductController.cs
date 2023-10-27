549b990c020c7522e48e1f4baff40b6745c157e4
FropZ <limtangmeng100@gmail.com>
2023-10-27 09:18:16 +0700

    Removed: logger

--- server/Controllers/ProductController.cs
+++ server/Controllers/ProductController.cs
@@ -20,21 +20,18 @@ namespace server.Controllers
         [HttpGet]
         public ActionResult<Result<IEnumerable<Product>>> GetProducts()
         {
-            _logger.LogInformation("GET: Products");
             return Ok(new Result<IEnumerable<Product>>(_productService.GetProducts().Result));
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Result<Product?>> GetProductById(int id)
+        public ActionResult<Result<Product?>> GetProductById(string id)
         {
-            _logger.LogInformation($"GET: Product by id = {id}");
             return Ok(new Result<Product?>(_productService.GetProductById(id).Result));
         }
 
         [HttpPost]
         public ActionResult<Result<Product?>> CreateProduct([FromBody] ProductCreated product)
         {
-            _logger.LogInformation("CREATE: Product");
             return Ok(new Result<Product?>(_productService.AddProduct(product).Result));
         }
 
@@ -43,7 +40,6 @@ namespace server.Controllers
             [FromBody] List<ProductUpdated> products
         )
         {
-            _logger.LogInformation("UPDATE: Products");
             return Ok(
                 new Result<IEnumerable<Product>>(_productService.UpdateProducts(products).Result)
             );
@@ -51,19 +47,17 @@ namespace server.Controllers
 
         [HttpPatch("{id}")]
         public ActionResult<Result<Product?>> UpdateProductById(
-            int id,
+            string id,
             [FromBody] ProductUpdated product
         )
         {
-            _logger.LogInformation($"UPDATE: Product by ID = {id}");
             return Ok(new Result<Product?>(_productService.UpdateProductById(id, product).Result));
         }
 
         [HttpDelete("{id}")]
-        public ActionResult<Result<bool>> DeleteProductById(int id)
+        public ActionResult<Result<bool>> DeleteProductById(string id)
         {
-            _logger.LogInformation($"DELETE: Product by ID = {id}");
-            return Ok(new Result<bool>(_productService.DeleteProduct(id).Result));
+            return Ok(new Result<bool>(_productService.DeleteProductAsync(id).Result));
         }
     }
 }
